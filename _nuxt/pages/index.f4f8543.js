889ece23802685644db6f20303e54ff7200e9b79
zhougonglai <zhougonglai@users.noreply.github.com>
2020-07-22 02:05:55 +0000

    deploy: a74933990e98f4d444fb3d1d16508213ac8a1e85

---
+++ _nuxt/pages/index.f4f8543.js
@@ -0,0 +1,1 @@
+(window.webpackJsonp=window.webpackJsonp||[]).push([[2],{250:function(t,e,n){"use strict";n.r(e);var l=n(14),component=Object(l.a)({},(function(){var t=this.$createElement;return(this._self._c||t)("div",{attrs:{id:"home"}},[this._v("index")])}),[],!1,null,null,null);e.default=component.exports}}]);
